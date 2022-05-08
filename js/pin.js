cc627f07d013d439e1bb0ec71ad51747a0b160cc
alxk <98492373+alxk05@users.noreply.github.com>
2022-05-08 11:45:33 -0700

    Add files via upload

---
+++ js/pin.js
@@ -0,0 +1,21 @@
+// onClick pinbutton, store note1 or note2 to pin1 or pin2 on client-side localStorage.
+
+function pin1() {
+  var x = document.getElementById("txtinput1").value;
+  localStorage.setItem("pin1", x) ;
+  document.getElementById("txtoutput1").innerHTML = x;
+  alert(x);
+}
+
+
+function pin2() {
+  var y = document.getElementById("txtinput2").value;
+  localStorage.setItem("pin2", y) ;
+  document.getElementById("txtoutput2").innerHTML = y;
+  alert(y);
+}
+
+
+function clear() {
+  document.getElementById("output").value = "";
+}
