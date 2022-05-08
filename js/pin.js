b97dc3bdf292bc01cdae18d954ef74adef80f035
alxk <98492373+alxk05@users.noreply.github.com>
2022-05-08 13:06:28 -0700

    Update pin.js

--- js/pin.js
+++ js/pin.js
@@ -2,15 +2,22 @@
 
 function pin1() {
   var x = document.getElementById("txtinput1").value;
-  localStorage.setItem("pin1", x) ;
+  localStorage.setItem("pin1", x);
+  alert("NOTE I. successfully pinned");
   document.getElementById("txtoutput1").innerHTML = x;
-  alert(x);
 }
 
 
 function pin2() {
   var y = document.getElementById("txtinput2").value;
-  localStorage.setItem("pin2", y) ;
+  localStorage.setItem("pin2", y);
+  alert("NOTE II. successfully pinned");
   document.getElementById("txtoutput2").innerHTML = y;
-  alert(y);
+}
+
+// download txt to file on client's local harddrive
+
+function dl(x) {
+  function dataUrl(data) {return "data:x-application/text," + escape(data);}
+  window.open(dataUrl(x));
 }
